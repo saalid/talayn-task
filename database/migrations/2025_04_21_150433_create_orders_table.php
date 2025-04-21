15804109ad8c315296bec216d1bb7bbcd0eff682
saalid <amirrezasalari1997@gmail.com>
2025-04-21 18:43:39 +0330

    faet(models): create models and create migrations of tables

---
+++ database/migrations/2025_04_21_150433_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('type', ['buy', 'sell']);
+            $table->decimal('amount', 10, 3);
+            $table->bigInteger('price');
+            $table->decimal('remaining_amount', 10, 3);
+            $table->enum('status', ['open', 'filled', 'cancelled'])->default('open');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2025_04_21_150443_create_trades_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trades', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('buy_order_id')->constrained('orders')->onDelete('cascade');
+            $table->foreignId('sell_order_id')->constrained('orders')->onDelete('cascade');
+            $table->foreignId('buyer_id')->constrained('users');
+            $table->foreignId('seller_id')->constrained('users');
+            $table->decimal('amount', 10, 3);
+            $table->bigInteger('price');
+            $table->bigInteger('fee_buyer');
+            $table->bigInteger('fee_seller');
+            $table->bigInteger('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trades');
+    }
+};
